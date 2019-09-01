8912e729813d584a1f09d0805e5fda46afb45c80
erwanledoux <lawanledoux@gmail.com>
2019-09-01 14:37:51 +0200

    export also exports from fetch-normalize-data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+import {
+  ASSIGN_DATA,
+  DELETE_DATA,
+  MERGE_DATA,
+  RESET_DATA,
+  SET_DATA,
+  assignData,
+  deleteData,
+  fetchData,
+  getNormalizedDeletedState,
+  getNormalizedMergedState,
+  mergeData,
+  resetData,
+  setData
+} from 'fetch-normalize-data'
+
+export {
+  ASSIGN_DATA,
+  DELETE_DATA,
+  MERGE_DATA,
+  RESET_DATA,
+  SET_DATA,
+  assignData,
+  deleteData,
+  fetchData,
+  getNormalizedDeletedState,
+  getNormalizedMergedState,
+  mergeData,
+  resetData,
+  setData
+}
+
 export * from './createDataReducer'
 export * from './requestData'
